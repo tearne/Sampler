110bd195116e16acdbc775fc960507c1a2dcab5e
Ashley Goddard <adg6hl@hotmail.com>
2012-09-05 16:21:59 +0100

    started work on a second ScriptRunner test, testing a file output from R

--- src/test/scala/sampler/r/wrap/ScriptRunnerSpec.scala
+++ src/test/scala/sampler/r/wrap/ScriptRunnerSpec.scala
@@ -17,15 +17,14 @@ class ScriptRunnerSpec extends Specification {
 	"ScriptRunner" should {
 		
 		"take at least 2 seconds to run when the R sleep command is used" in new fileTearDown {
-			val startTime = System.nanoTime()
+			val startTime = System.nanoTime
 			
-			val scriptRunner = new ScriptRunner()
+			val scriptRunner = new ScriptRunner
 			
-			val builder = new StringBuilder()
+			val builder = new StringBuilder
 			builder.append("a<-1+1\n")
 			builder.append("Sys.sleep(2)\n")
 			builder.append("b<-2+2\n")
-			
 			
 			scriptRunner.apply(builder.toString(), testPath)
 			
@@ -36,8 +35,27 @@ class ScriptRunnerSpec extends Specification {
 			runTime must beGreaterThan(2.0)
 		}
 		
-		"produce a file containing JSON when the RJSON library is used" in todo
-		
+		"produce a file containing JSON when the RJSON library is used" in new fileTearDown{
+			val scriptRunner = new ScriptRunner
+			
+			val builder = new StringBuilder
+			
+			builder.append("library(\"rjson\")\n")
+			builder.append("a <- c(1,2,3)\n")
+			builder.append("df <- as.data.frame(a)\n")
+			builder.append("names(df) <- c(\"parameter\")\n")
+			builder.append("jsonOut <- toJSON(df)\n")
+			builder.append("fileName <- file(\"" + "myJSON.txt" + "\")\n")
+			builder.append("writeLines(jsonOut, fileName)\n")
+			builder.append("close(fileName)\n")
+
+			scriptRunner.apply(builder.toString(), testPath)
+			
+//			TODO read in JSON and test contents
+//			TODO delete JSON file after test execution
+			
+			Nil
+		}
 	}
 	
 	trait fileTearDown extends After {
