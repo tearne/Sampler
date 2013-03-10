ad0511c63c0d01ec1facf0240f272375976347a8
AHVLA <ahvla@chuusai.com>
2013-03-10 14:12:58 +0000

    Added type annotations to allow forward references.

--- src/main/scala/sampler/data/Types.scala
+++ src/main/scala/sampler/data/Types.scala
@@ -41,12 +41,12 @@ object Types {
 			else None
 		}
 	}
-	implicit val IntColumn = new ColumnType[Int] { def apply(s: String) = s.toInt }
-	implicit val DoubleColumn = new ColumnType[Double] { def apply(s: String) = s.toDouble }
-	implicit val BooleanColumn = new ColumnType[Boolean] { def apply(s: String) = s.toBoolean }
-	implicit val StringColumn = new ColumnType[String] { def apply(s: String) = s }
-	implicit val FactorColumn = new ColumnType[Factor] { def apply(s: String) = Factor(s) }
-	implicit val ProbabilityColumn = new ColumnType[Probability] { 
+	implicit val IntColumn: ColumnType[Int] = new ColumnType[Int] { def apply(s: String) = s.toInt }
+	implicit val DoubleColumn: ColumnType[Double] = new ColumnType[Double] { def apply(s: String) = s.toDouble }
+	implicit val BooleanColumn: ColumnType[Boolean] = new ColumnType[Boolean] { def apply(s: String) = s.toBoolean }
+	implicit val StringColumn: ColumnType[String] = new ColumnType[String] { def apply(s: String) = s }
+	implicit val FactorColumn: ColumnType[Factor] = new ColumnType[Factor] { def apply(s: String) = Factor(s) }
+	implicit val ProbabilityColumn: ColumnType[Probability] = new ColumnType[Probability] { 
 		def apply(s: String) = Probability(s.toDouble)
 				override def toString(p: Probability) = p.value.toString
 	}
