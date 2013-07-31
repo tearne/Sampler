79375470e3abbd4801afdc7bcd81b5a7aacdc87e
Ashley Goddard <ashley.goddard@ahvla.gsi.gov.uk>
2013-07-31 14:25:47 +0100

    Documented and tested the Statistics Component trait

--- core/src/test/scala/sampler/math/StasisticComponentTest.scala
+++ core/src/test/scala/sampler/math/StasisticComponentTest.scala
@@ -12,6 +12,16 @@ class StasisticComponentTest extends AssertionsForJUnit with StatisticsComponent
   implicit val r = Random
   val tolerance = 1e-8
   
+  @Test def testRightTail {
+	val empSeq = IndexedSeq[Double](1,2,3,4).toEmpiricalSeq
+    
+	assert(rightTail(empSeq, 1.0).value === 1.0)
+	assert(rightTail(empSeq, 2.0).value === 0.75)
+	assert(rightTail(empSeq, 3.0).value === 0.5)
+	assert(rightTail(empSeq, 4.0).value === 0.25)
+	assert(rightTail(empSeq, 5.0).value === 0)
+  }
+  
   @Test def calculatesEmpiricalSeqMean {
     val empSeq = IndexedSeq[Double](1,2,3,4).toEmpiricalSeq
 	
@@ -35,40 +45,102 @@ class StasisticComponentTest extends AssertionsForJUnit with StatisticsComponent
     mean(empWeight) should be(2.5 plusOrMinus tolerance)
   }
   
-//  @Test def calculatesEmpiricalSeqQuantile {
-//    val empSeq = IndexedSeq[Double](1,2,3,4,5).toEmpiricalSeq
-//    
-//    print(s"${quantile(empSeq, Probability(0))}, ")
-//    print(s"${quantile(empSeq, Probability(0.25))}, ")
-//    print(s"${quantile(empSeq, Probability(0.5))}, ")
-//    print(s"${quantile(empSeq, Probability(0.75))}, ")
-//    print(s"${quantile(empSeq, Probability(1.0))}\n")
-//  }
-//  
-//  @Test def calculatesEmpiricalTableQuantile {
-//	val empTable = IndexedSeq[Double](1,2,3,4,5).toEmpiricalTable
-//			  
-//	print(s"${quantile(empTable, Probability(0))}, ")
-//	print(s"${quantile(empTable, Probability(0.25))}, ")
-//	print(s"${quantile(empTable, Probability(0.5))}, ")
-//	print(s"${quantile(empTable, Probability(0.75))}, ")
-//	print(s"${quantile(empTable, Probability(1.0))}\n")
-//  }
+  @Test def quantileSmallExampleEmpiricalSeq {
+    val empSeq = IndexedSeq[Double](1,2,3,4).toEmpiricalSeq
+    
+    assert(quantile(empSeq, Probability(0.25)) === 1.0)
+    assert(quantile(empSeq, Probability(0.5)) === 2.0)
+    assert(quantile(empSeq, Probability(0.75)) === 3.0)
+  }
   
-  @Test def calculatesEmpiricalWeightQuantile {
+  @Test def quantileSmallExampleEmpiricalTable {
+    val empTable = IndexedSeq[Double](1,2,2,3,4).toEmpiricalTable
+    
+    assert(quantile(empTable, Probability(0.25)) === 2.0)
+    assert(quantile(empTable, Probability(0.5)) === 2.0)
+    assert(quantile(empTable, Probability(0.75)) === 3.0)
+  }
+  
+  @Test def quantileEmpiricalSeq12Primes {
+    val empSeq = IndexedSeq[Double](2,3,5,7,11,13,17,19,23,27,31,37).toEmpiricalSeq
+    
+    assert(quantile(empSeq, Probability(0.25)) === 5.0)
+    assert(quantile(empSeq, Probability(0.5)) === 13.0)
+    assert(quantile(empSeq, Probability(0.75)) === 23.0)
+  }
+
+  @Test def quantileEmpiricalTable11Primes {
+	val empTable = IndexedSeq[Double](2,3,5,7,11,13,17,19,23,27,31).toEmpiricalSeq
+			  
+	assert(quantile(empTable, Probability(0.25)) === 5.0)
+	assert(quantile(empTable, Probability(0.5)) === 13.0)
+	assert(quantile(empTable, Probability(0.75)) === 23.0)
+  }
+
+  @Test def quantileEmpiricalSeq10Primes {
+	val empSeq = IndexedSeq[Double](2,3,5,7,11,13,17,19,23,27).toEmpiricalSeq
+			  
+	assert(quantile(empSeq, Probability(0.25)) === 5.0)
+	assert(quantile(empSeq, Probability(0.5)) === 11.0)
+	assert(quantile(empSeq, Probability(0.75)) === 19.0)
+  }
+
+  @Test def quantileEmpiricalTable9Primes {
+	val empTable = IndexedSeq[Double](2,3,5,7,11,13,17,19,23).toEmpiricalSeq
+			  
+	assert(quantile(empTable, Probability(0.25)) === 5.0)
+	assert(quantile(empTable, Probability(0.5)) === 11.0)
+	assert(quantile(empTable, Probability(0.75)) === 17.0)
+  }
+  
+  @Test def quantileEmpiricalWeightSmallExample {
     val empWeight = Map[Double, Double](
 		(1, 0.2),
-		(2, 0.2),
-		(2, 0.2),
+		(2, 0.4),
 		(3, 0.2),
 		(4, 0.2)
       ).toEmpiricalWeighted
+	
+    assert(quantile(empWeight, Probability(0.25)) === 2.0)
+    assert(quantile(empWeight, Probability(0.5)) === 2.0)
+    assert(quantile(empWeight, Probability(0.75)) === 3.0)
+  }
+  
+  @Test def quantileEmpiricalWeightTwelvePrimes {
+	  val empWeight = Map[Double, Double](
+		(2, 0.1),
+		(3, 0.1),
+		(5, 0.1),
+		(7, 0.1),
+		(11, 0.1),
+		(13, 0.1),
+		(17, 0.1),
+		(19, 0.1),
+		(23, 0.1),
+		(27, 0.1),
+		(31, 0.1),
+		(37, 0.1)
+	  ).toEmpiricalWeighted
 			  
-	print(s"${quantile(empWeight, Probability(0))}, ")
-	print(s"${quantile(empWeight, Probability(0.25))}, ")
-	print(s"${quantile(empWeight, Probability(0.5))}, ")
-	print(s"${quantile(empWeight, Probability(0.75))}, ")
-	print(s"${quantile(empWeight, Probability(1.0))}")
+	assert(quantile(empWeight, Probability(0.25)) === 5.0)
+	assert(quantile(empWeight, Probability(0.5)) === 13.0)
+	assert(quantile(empWeight, Probability(0.75)) === 23.0)
+  }
+  
+  @Test def quantileAcceptSeqOfLengthOne {
+    val empSeq = IndexedSeq[Double](1.0).toEmpiricalSeq
+    
+    assert(quantile(empSeq, Probability(0.25)) === 1.0)
+	assert(quantile(empSeq, Probability(0.5)) === 1.0)
+	assert(quantile(empSeq, Probability(0.75)) === 1.0)
+  }
+  
+  @Test def quantileAssertionErrorWhenSeqOfLengthZero {
+    val empSeq = IndexedSeq[Double]().toEmpiricalSeq
+    
+    intercept[AssertionError] {
+      quantile(empSeq, Probability(0.25))
+    }
   }
   
   @Test def calcualatesAbsoluteDifferenceMetric {
