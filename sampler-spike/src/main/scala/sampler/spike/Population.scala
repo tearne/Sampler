d1a77cd1f96183cdfe0de65b536c66433f8fdf48
Ashley Goddard <ashley.goddard@ahvla.gsi.gov.uk>
2013-10-03 16:43:42 +0100

    First draft of a population class and tests

---
+++ sampler-spike/src/main/scala/sampler/spike/Population.scala
@@ -0,0 +1,46 @@
+package sampler.spike
+
+import scala.annotation.tailrec
+import sampler.math.Random
+
+class Population[T](pop: Map[T, Int])(implicit r: Random) {
+	
+  private def expand(s: T, i: Int) = (1 to i).map(_ => s)
+  
+  private lazy val flatPop = pop.flatMap(x => expand(x._1,x._2)).toIndexedSeq
+  
+  private lazy val popSize = flatPop.size
+  
+  private def checkSize(size: Int) = {
+	  assert(size>0, "Cannot sample 0 or less objects")
+	  assert(size<=popSize, "Cannot sample more elements than there are in the population")
+  }
+  
+  private def reMap(l: List[T]) = {
+    l.distinct.map(a => (a -> l.count(_ == a))).toMap
+  }
+  
+  def sampleWithoutReplacement(sampleSize: Int) = {
+    checkSize(sampleSize)
+    
+	@tailrec
+    def takeAnother(acc: List[T], bag: IndexedSeq[T]): List[T] = {
+	  if(acc.size == sampleSize) acc
+	  else{ 
+		val item = bag(r.nextInt(bag.size))
+		takeAnother(item +: acc, bag diff List(item))
+	  }
+	}
+	
+	val selected = takeAnother(Nil, flatPop)
+	val remaining = (flatPop diff selected).toList
+	
+	(reMap(selected), reMap(remaining))
+  }
+  
+  def sampleWithReplacement(sampleSize: Int) = {
+    checkSize(sampleSize) //TODO
+    
+    reMap((1 to sampleSize).map(_ => flatPop(r.nextInt(popSize))).toList)
+  }
+}
---
+++ sampler-spike/src/test/scala/sampler/spike/PopulationTest.scala
@@ -0,0 +1,112 @@
+package sampler.spike
+
+import sampler.math.Random
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito._
+import org.mockito.Mockito
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.matchers.ShouldMatchers
+
+class PopulationTest extends AssertionsForJUnit with MockitoSugar with ShouldMatchers {
+
+  implicit var r: Random = _
+
+  @Before 
+  def initialise = {
+    r = mock[Random]
+  }
+  
+  @Test
+  def samplesWithoutReplacement {
+    when(r.nextInt(10)).thenReturn(1,1,1)
+    
+	val p = new Population(Map("A" -> 1, "B" -> 9))
+    
+    val samples = p.sampleWithoutReplacement(3)._1
+    
+    assert(samples.getOrElse("A", 0) === 1)
+    assert(samples.getOrElse("B", 0) === 2)
+  }
+  
+  @Test
+  def returnsRemainingMapWithoutReplacement {
+    when(r.nextInt(10)).thenReturn(1,1,1)
+    
+	val p = new Population(Map("A" -> 1, "B" -> 9))
+    
+    val remaining = p.sampleWithoutReplacement(3)._2
+    
+    assert(remaining.getOrElse("A", 0) === 0)
+    assert(remaining.getOrElse("B", 0) === 7)
+  }
+  
+  @Test
+  def neverReplacesWhenSamplingWithReplacement {
+    implicit val random = Random
+    val p = new Population(Map("A" -> 1, "B" -> 9))(random)
+    
+    (1 to 100).map{a =>
+      val samples = p.sampleWithoutReplacement(10)._1
+      
+      assert(samples.getOrElse("A", 1) === 1)
+    }
+  }
+  
+  @Test
+  def samplesWithReplacement {
+    when(r.nextInt(10)).thenReturn(3)
+    
+	val p = new Population(Map("A" -> 1, "B" -> 9))
+    
+    val samples = p.sampleWithReplacement(3)
+    
+    assert(samples.getOrElse("A", 0) === 0)
+    assert(samples.getOrElse("B", 0) === 3)
+  }
+  
+  @Test
+  def samplesWithReplacementWithCorrectProbabilities {
+    implicit val random = Random
+    val p = new Population(Map("A" -> 1, "B" -> 9))(random)
+    
+    def countUp[T](pop: Population[T], n: Integer, acc: Map[String, Int]): Map[String, Int] = {
+      if(n == 1000) acc
+      else {
+        val samples = p.sampleWithReplacement(10)
+        
+        val a = samples.getOrElse("A", 0) + acc.getOrElse("A", 0)
+        val b = samples.getOrElse("B", 0) + acc.getOrElse("B", 0)
+        
+        countUp(pop, n+1, Map("A" -> a, "B" -> b))
+      }
+    }
+    
+    val result = countUp(p, 0, Map("A" -> 0, "B" -> 0))
+    
+    result.getOrElse("A", 0) should be(1000 plusOrMinus 50)
+    result.getOrElse("B", 0) should be(9000 plusOrMinus 50)
+  }
+  
+  @Test
+  def failIfSampleSizeBelowZeroOrTooLarge {
+    val p = new Population(Map("A" -> 1, "B" -> 9))
+    
+    intercept[AssertionError] {
+      p.sampleWithoutReplacement(11)
+    }
+
+    intercept[AssertionError] {
+    	p.sampleWithoutReplacement(-1)
+    }
+    
+    intercept[AssertionError] {
+      p.sampleWithReplacement(11)
+    }
+    
+    intercept[AssertionError] {
+    	p.sampleWithReplacement(-1)
+    }
+  }
+}
