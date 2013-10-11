b122b7b49b5b586017a54c029b86c79bf492f0eb
Ashley Goddard <ashley.goddard@ahvla.gsi.gov.uk>
2013-10-11 08:58:29 +0100

    Added Scaladoc to the ScriptRunner

--- sampler-core/src/main/scala/sampler/r/ScriptRunner.scala
+++ sampler-core/src/main/scala/sampler/r/ScriptRunner.scala
@@ -8,7 +8,14 @@ import scala.io.Source
 
 // Should QuickPlot be in companion object?
 
+/** A class to allow the running of an R script from within the Scala environment*/
 class ScriptRunner {
+	/** Writes and R script to the location defined by path. Executes the script using 
+	 *  the Rscript shell script
+	 * 	
+	 *  @param script The R script to be executed
+	 *  @param scriptPath The path to where the script should be written, including filename
+	 */
 	def apply(script: String, scriptPath: Path){
 		val writer = new FileWriter(scriptPath.toFile)
 		val parentPath = scriptPath.toAbsolutePath.getParent.toFile
@@ -30,6 +37,7 @@ class ScriptRunner {
 	}
 }
 
+/** Companion object to allow running of an R script*/
 object ScriptRunner{
 	lazy val instance = new ScriptRunner
 	def apply(script: String, scriptTarget: Path) = instance(script, scriptTarget) 
