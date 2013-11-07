28dbccb8c73e000bc118e592eaed3999c2c3391b
tearne <tearne@gmail.com>
2013-11-07 10:40:36 +0000

    Remove old master - slave ABC runner

--- sampler-cluster/src/main/scala/sampler/cluster/abc/master/Messages.scala
+++
@@ -1,9 +0,0 @@
-package sampler.cluster.abc.master
-
-import scala.language.existentials
-import akka.actor.ActorRef
-import sampler.cluster.abc.ABCJob
-
-case class ClusterBusy()
-case class WorkAvailable()
-case class AbortAll(id: Int)
