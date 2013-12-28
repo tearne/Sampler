0cc833983041a3ff418ffa1d1329fdc23fe328b4
tearne <tearne@gmail.com>
2013-12-28 06:49:57 +0000

    Prep for first attempt at actor testing

--- project/Build.scala
+++ project/Build.scala
@@ -51,6 +51,7 @@ object SamplerBuild extends Build{
 				"com.typesafe.akka" %% "akka-remote" % "2.3-M2",
 				"com.typesafe.akka" %% "akka-cluster" % "2.3-M2",
 				"com.typesafe.akka" %% "akka-slf4j" % "2.3-M2", 
+				"com.typesafe.akka" %% "akka-testkit" % "2.3-M2" % "test",
 				"org.apache.commons" % "commons-math3" % "3.0",
 				"com.amazonaws" % "aws-java-sdk" % "1.4.0.1",
 				"net.schmizz" % "sshj" % "0.9.0"
