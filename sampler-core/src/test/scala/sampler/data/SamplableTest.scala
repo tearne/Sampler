01e318ac048d61b49dad0023761ab346a6f8ecf0
Ashley Goddard <ashley.goddard@ahvla.gsi.gov.uk>
2014-02-26 14:17:06 +0000

    Fixed bug in SamplableMap removeOne method

--- sampler-core/src/test/scala/sampler/data/SamplableTest.scala
+++ sampler-core/src/test/scala/sampler/data/SamplableTest.scala
@@ -10,15 +10,21 @@ import sampler.math.StatisticsComponent
 
 class SamplableTest extends AssertionsForJUnit with MockitoSugar with ToSamplable {
   
+  class MyParticle[T](a: Char)
+    
+  val p1 = new MyParticle('a')
+  val p2 = new MyParticle('b')
+  val p3 = new MyParticle('c')
+  
+  trait Colour
+  object Red extends Colour { override def toString = "Red"}
+  object Green extends Colour { override def toString = "Green"}
+  object Blue extends Colour { override def toString = "Blue"}
+  
   @Test
   def canDrawMoreThanTheNumberOfObjectTypesInBag {
     implicit val r = mock[Random]
     when(r.nextInt(anyInt)).thenReturn(599,350,350,250) // Blue, Green, Green Red
-    
-    trait Colour
-    object Red extends Colour { override def toString = "Red"}
-    object Green extends Colour { override def toString = "Green"}
-    object Blue extends Colour { override def toString = "Blue"}
     
     val startBag = Map(Red -> 300, Green -> 200, Blue -> 100)
     
@@ -38,11 +44,6 @@ class SamplableTest extends AssertionsForJUnit with MockitoSugar with ToSamplabl
     implicit val r = mock[Random]
     when(r.nextInt(anyInt)).thenReturn(2,2) // Red, Green
     
-    trait Colour
-    object Red extends Colour { override def toString = "Red"}
-    object Green extends Colour { override def toString = "Green"}
-    object Blue extends Colour { override def toString = "Blue"}
-    
     val startSeq = IndexedSeq(Red, Red, Red, Green, Green)
     
     val(remaining, drawn) = startSeq.draw(2)
@@ -54,32 +55,52 @@ class SamplableTest extends AssertionsForJUnit with MockitoSugar with ToSamplabl
     assert(drawn.getOrElse(Green, 0) === 1)
   }
   
-  @Test
-  def lookingForBug {
-    // TODO not an actual test - delete me
-    implicit val r = Random
-    class MyParticle[T](i: T) {
-      override def toString = i.toString
-    }
+  @Test def samplesFromImplicitMapCorrectly {
+    implicit val r = mock[Random]
     
-    val p1 = new MyParticle(1)
-    val p2 = new MyParticle(2)
-    val p3 = new MyParticle(3)
+    when(r.nextInt(3)).thenReturn(2)
+    when(r.nextInt(2)).thenReturn(1)
     
-    val oneOfEach = Seq(p1,p2,p3).map{
+    val implicitMap = IndexedSeq(p1,p2,p3).map{
       case (particle) => particle -> 1
     }.toMap
     
-    // TODO fix not working implicit
-//    val res = oneOfEach.draw(2)._2
+    val result = implicitMap.draw(2)._2
+    assert(result.getOrElse(p1, 0) === 0)
+    assert(result.getOrElse(p2, 0) === 1)
+    assert(result.getOrElse(p3, 0) === 1)
+  }
+  
+  @Test
+  def samplingFromDefinedMapCorrectly {
+    implicit val r = mock[Random]
     
-    val sMap = new SamplableMap(oneOfEach)
+    when(r.nextInt(4)).thenReturn(2)
+    when(r.nextInt(3)).thenReturn(2)
+    when(r.nextInt(2)).thenReturn(0)
     
-    // TODO third particle never selected - fix this
-    (0 to 100).map{_ =>
-      val result = sMap.draw(2)._2
-      if(result.keySet.contains(p3)) println("Contains 3")
-    }
+    val sMap = new SamplableMap(Map(p1 -> 1, p2 -> 2, p3 -> 1))
+
+    val result = sMap.draw(3)._2
     
+    assert(result.getOrElse(p1, 0) === 1)
+    assert(result.getOrElse(p2, 0) === 1)
+    assert(result.getOrElse(p3, 0) === 1)
+  }
+  
+  @Test
+  def lookingForBug {
+    implicit val r = mock[Random]
+    
+    when(r.nextInt(3)).thenReturn(2)
+    when(r.nextInt(2)).thenReturn(1)
+    
+    val implicitSeq = IndexedSeq(p1,p2,p3)
+    
+    val result = implicitSeq.draw(2)._2
+   
+    assert(result.getOrElse(p1, 0) === 0)
+    assert(result.getOrElse(p2, 0) === 1)
+    assert(result.getOrElse(p3, 0) === 1)
   }
 }
