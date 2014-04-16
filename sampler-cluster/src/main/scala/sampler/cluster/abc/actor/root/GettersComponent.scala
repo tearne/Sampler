168e43c2f4c9d729289991123bdf0e4368db458b
tearne <tearne@gmail.com>
2014-04-16 11:25:10 +0100

    Restore logging which had been disabled since caused tests to fail.

--- sampler-cluster/src/main/scala/sampler/cluster/abc/actor/root/GettersComponent.scala
+++ sampler-cluster/src/main/scala/sampler/cluster/abc/actor/root/GettersComponent.scala
@@ -2,6 +2,8 @@ package sampler.cluster.abc.actor.root
 
 import sampler.cluster.abc.actor.Lenses
 import sampler.cluster.abc.config.ABCConfig
+import sampler.cluster.abc.actor.WeighedParticles
+import sampler.cluster.abc.algorithm.Generation
 
 trait GettersComponent{
 	val getters: Getters
@@ -10,4 +12,9 @@ trait GettersComponent{
 trait Getters {
 	def getMixRateMS(config: ABCConfig): Long = config.cluster.mixRateMS
 	def getNumParticles(config: ABCConfig): Int = config.job.numParticles
+	
+	//TODO so far these are just used in logging, to avoid errors when testing, but they could
+	// be used more widely, or perhaps converted to lenses?
+	def getNumParticles[P](weighedParticles: WeighedParticles[P]): Int = weighedParticles.seq.size
+	def getAccumulatedGenerationSize[P](generation: Generation[P]): Int = generation.weighted.size
 }
