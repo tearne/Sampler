74b6b9818f078dcdeaa36ad08a886ac7a4f8853e
tearne <tearne@gmail.com>
2015-02-14 16:26:13 +0000

    Restructure for multiple Farrington runs

--- sampler-spike/src/main/scala/sampler/spike/farrington/EDS.scala
+++ sampler-spike/src/main/scala/sampler/spike/farrington/EDS.scala
@@ -2,29 +2,39 @@ package sampler.spike.farrington
 
 import java.nio.file.{Files,Paths}
 import java.time.YearMonth
-import java.time.temporal.ChronoUnit.MONTHS
 import scala.annotation.elidable.ASSERTION
 import scala.collection.immutable.TreeMap
 import scala.io.Source
-import scala.language.postfixOps
-import org.json4s.DefaultFormats
+import scala.language.{postfixOps,implicitConversions}
 import org.json4s.JsonDSL._
 import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.rosuda.REngine.Rserve.RConnection
 import sampler.r.rserve.RServeHelper
+import java.time._
+import java.time.Year
+import org.threeten.extra.Interval
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit._
+import java.time.temporal.ChronoField
+import java.time.Duration.of
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+import java.time.temporal.TemporalUnit
+import scala.collection.SortedMap
+import org.json4s.JsonAST.JValue
+import org.json4s.native.JsonMethods
 
 object EDS extends App{
 	val resultsDir = Paths.get("results", "farrington")
 	Files.createDirectories(resultsDir)
 	
 	val cl = getClass.getClassLoader
-	val rScript = Source.fromURI(cl.getResource("farrington/script.r").toURI()).mkString
 	val input = cl.getResource("farrington/input.txt").toURI()
 	
-	implicit val formats = DefaultFormats
+	val exclude2001 = (1 to 12).map{m => YearMonth.of(2001, m)}.to[Set]
 	
-	val dataIn = TreeMap{
+	val countData = TreeMap{
 		Source.fromFile(input)
 			.getLines()
 			.filter(_.trim.size > 0)
@@ -39,70 +49,50 @@ object EDS extends App{
 			.toSeq: _*
 	}
 	
-	assert(!dataIn.exists(_._1.getYear == 2001)) 
-	val first = dataIn.head._1
-	val last = dataIn.last._1
-	val window = List(-1, 0, 1).map(v => (v + 12) % 12)
+	RServeHelper.ensureRunning()
+	val rCon = new RConnection
+	try{
+		val indexedData = indexAndExclude(countData, exclude2001)
 	
-	def isBaseline(date: YearMonth) = {
-		def isWithinWindow() = {
-			val monthRemainder = MONTHS.between(date, last) % 12
-			val inWindow = window.exists(_ == monthRemainder)
-      val inYrRangeMin = MONTHS.between(date, last) <= 12*12+1
-      val inYrRangeMax = MONTHS.between(date, last) > 2
-      inWindow && inYrRangeMin && inYrRangeMax
+		(0 to 10).map{i => 
+			val series = extractWindow(indexedData.dropRight(i))
+			println(Farrington.run(series, rCon))
 		}
-		date.getYear != 2001 && isWithinWindow
+	} finally {
+		rCon.close
+		RServeHelper.shutdown
 	}
 	
-	val (bDates, bCounts) = dataIn.filterKeys(isBaseline).unzip	
-  val (cDate, cCount) = dataIn.last
-  
-  bDates.zip(bCounts).foreach(println)
-  println(s"Current = $cDate, $cCount")
+	def indexAndExclude(
+			obsByDate: SortedMap[YearMonth, Int], 
+			exclusions: Set[YearMonth] = Set.empty
+	): SortedMap[Date, Int] = {
+		assert(!obsByDate.exists{case (ym, _) => exclusions.contains(ym)})
+		
+		val removedExclusions = obsByDate.filterKeys{ym => !exclusions.contains(ym)}
+		val firstDate = removedExclusions.firstKey
+		
+		implicit val dateOrdering = Ordering.by{d: Date => d.idx}
+		
+		removedExclusions.map{case (ym, count) => Date(ym, MONTHS.between(firstDate, ym)) -> count}
+	}
 	
-  //TODO change first?
-  
-	val json = 
-    ("Current" -> 
-      ("Month" -> MONTHS.between(first, cDate)) ~
-      ("Incidents" -> cCount)
-    ) ~
-    ("Baseline" ->
-      ("basemth" -> bDates.map{d => MONTHS.between(first, d)}) ~
-      ("basecont"-> bCounts)
-    )
-    
-  val jsonAsString = pretty(render(json))
-  val writer = Files.newBufferedWriter(Paths.get("tmp.json"))
-  writer.write(jsonAsString)
-  writer.newLine
-  writer.close
-    
-  println(jsonAsString)
-	
-	RServeHelper.ensureRunning()
-	val r = new RConnection
-	val rExp = 
-		try{
-			r.parseAndEval("""library(rjson)""")
-			r.assign("jsonIn", compact(render(json)))
-			r.parseAndEval("basedata = as.data.frame(fromJSON(jsonIn)$Baseline)")
-      r.parseAndEval("currentCount = as.data.frame(fromJSON(jsonIn)$Current$Incidents)")
-      r.parseAndEval("currentmth = as.data.frame(fromJSON(jsonIn)$Current$Month)")
-			r.parseAndEval(rScript)
-			r.parseAndEval("output")
-		}finally{
-			r.close
-			RServeHelper.shutdown
+	def extractWindow(timeSeries: SortedMap[Date, Int]): SortedMap[Date, Int] = {
+		val lastObsDate = timeSeries.lastKey
+		val window = List(-1, 0, 1).map(v => (v + 12) % 12)
+		val windowLowerBound = lastObsDate.yearMonth.minus(12, YEARS).minus(1, MONTHS)
+		
+		def keep(date: Date) = {
+			val monthRemainder = MONTHS.between(date.yearMonth, lastObsDate.yearMonth) % 12
+			val inWindow = window.exists(_ == monthRemainder)
+      
+      val isAfterStartDate = windowLowerBound.compareTo(date.yearMonth) <= 0 
+      val isBeforeEndDate = MONTHS.between(date.yearMonth, lastObsDate.yearMonth) > 2
+      val isBaseline = inWindow && isAfterStartDate && isBeforeEndDate
+      
+      isBaseline || date == lastObsDate
 		}
-	
-	val rOut = parse(rExp.asString())
-	println("------- JSON OUT -------")
-	println(pretty(render(rOut)))
-	println("--------- END ----------")
-
-case class MyRes(expected: Double, threshold: Double, trend: Int, exceed: Double, weights: List[Double])
-	val res = rOut.extract[MyRes]
-	println(res)
+		val t = timeSeries.filterKeys(keep)
+		t
+	}
 }
