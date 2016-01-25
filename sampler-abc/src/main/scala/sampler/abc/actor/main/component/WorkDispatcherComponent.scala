e6fc5e2e4714caeba7abdf7a1e93fa3fcd3d4715
thomdoherty <thomdoherty@yahoo.com>
2016-01-25 10:10:51 +0000

    No longer used

--- sampler-abc/src/main/scala/sampler/abc/actor/main/component/WorkDispatcherComponent.scala
+++ sampler-abc/src/main/scala/sampler/abc/actor/main/component/WorkDispatcherComponent.scala
@@ -1,15 +1,15 @@
-package sampler.abc.actor.main.component
-
-import akka.actor.Actor
-import scala.concurrent.ExecutionContext
-
-trait WorkDispatcherComponentImpl extends WorkDispatcherComponent {
-	this: Actor => 
-		
-	val workDispatcher = 
-		context.system.dispatchers.lookup("sampler.work-dispatcher")
-}
-
-trait WorkDispatcherComponent {
-	val workDispatcher: ExecutionContext
-}
+//package sampler.abc.actor.main.component
+//
+//import akka.actor.Actor
+//import scala.concurrent.ExecutionContext
+//
+//trait WorkDispatcherComponentImpl extends WorkDispatcherComponent {
+//	this: Actor => 
+//		
+//	val workDispatcher = 
+//		context.system.dispatchers.lookup("sampler.work-dispatcher")
+//}
+//
+//trait WorkDispatcherComponent {
+//	val workDispatcher: ExecutionContext
+//}
