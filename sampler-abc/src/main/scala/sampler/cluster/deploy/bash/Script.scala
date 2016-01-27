5d3926241fa85350c6cd6a15e75a952e252c67ff
thomdoherty <thomdoherty@yahoo.com>
2016-01-27 15:43:38 +0000

    Hardcoding TBMIFit for now as parsing main class doesn't work

--- sampler-abc/src/main/scala/sampler/cluster/deploy/bash/Script.scala
+++ sampler-abc/src/main/scala/sampler/cluster/deploy/bash/Script.scala
@@ -7,9 +7,9 @@ object Script {
   
  // def killJava() = "killall java > killAllResult.txt 2>&1"
   //def killJava() = "pkill -f 'java.*TBMIFitApp'"
-  def killJava(mainClass: String) = "pkill -f 'java.*"+mainClass.split(".").tail+"'"  
+  def killJava(mainClass: String) = "pkill -f 'java.*TBMIFit'"//+mainClass.split(".").tail+"'"  
   
-  def checkJavaRunning(mainClass: String) = "jps | grep "+mainClass.split(".").tail+"'" //assumes jdk installed which it should be
+  def checkJavaRunning(mainClass: String) = "/root/deploy/jdk1.8.0_71/bin/jps | grep 'TBMIFit'"//+mainClass.split(".").tail+"'" //assumes jdk installed which it should be
   	
   //No longer needed if rsync uses --delete option
   //def deleteOld(dir: String) = s"rm $dir/run*.sh && rm -r $dir/log" 
@@ -33,13 +33,13 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $DIR
 """+
 s"""
-java \\
+/root/deploy/jdk1.8.0_71/bin/java \\
 ${vmExtraArgs.mkString(" ")} \\
 -Dakka.remote.netty.tcp.hostname=$hostIP \\
 -Dakka.cluster.seed-nodes.0=akka.tcp://ABC@$seedOneIP:2552 \\
 -Dakka.cluster.seed-nodes.1=akka.tcp://ABC@$seedTwoIP:2552 \\
 -Dconfig.file=application.conf \\
--Dlogback.configurationFile=logback.xml \\
+-Dlogback.configurationFile=/root/deploy/logback.xml \\
 -cp "lib/*" \\
 $mainClass
 """
