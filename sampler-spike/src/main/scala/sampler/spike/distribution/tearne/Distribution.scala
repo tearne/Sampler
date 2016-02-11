ff8ce3cb8bca0b246ed8fcaa0120a87e7e2d8595
Tearne <tearne@gmail.com>
2016-02-11 14:54:22 +0000

    Skeleton of initial intentions

--- sampler-spike/src/main/scala/sampler/spike/distribution/tearne/Distribution.scala
+++ sampler-spike/src/main/scala/sampler/spike/distribution/tearne/Distribution.scala
@@ -6,3 +6,11 @@ import scala.language.higherKinds
 trait Distribution[D[_]] extends Monad[D]{
   def fromTable[A](weights: Map[A, Double]): D[A]
 }
+
+object Discussion {
+  val table: Map[Item, Double] = ???
+  val observations: Seq[Items] = ???
+  
+  val d = observations.toDistirbubtion
+  val samples = (1 to 100).map{_ => d.sample}
+}
