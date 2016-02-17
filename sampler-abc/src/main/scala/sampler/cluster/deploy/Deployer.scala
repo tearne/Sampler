00fead1e3c7c28af0352f5b4a7508eeaca7652b4
TBFT5168 <oliver.tearne@apha.gsi.gov.uk>
2016-02-17 16:35:00 +0000

    Added new listIdle typeclass for command line

--- sampler-abc/src/main/scala/sampler/cluster/deploy/Deployer.scala
+++ sampler-abc/src/main/scala/sampler/cluster/deploy/Deployer.scala
@@ -3,70 +3,70 @@ package sampler.cluster.deploy
 import java.io.File
 import java.nio.file.Path
 import org.apache.commons.io.FileUtils
-import sampler.cluster.deploy.bash.{Rsync, SSH, Script}
+import sampler.cluster.deploy.bash.{ Rsync, SSH, Script }
 import scala.sys.process._
 import scopt.Read
 import org.slf4j.LoggerFactory
 
 object Deployer {
   val log = LoggerFactory.getLogger(getClass.getName)
-  val processLog = ProcessLogger{line => 
+  val processLog = ProcessLogger { line =>
     log.info(line)
   }
-  
+
   def apply(args: Array[String], providerBuilder: String => Provider): Unit = {
     parser
       .parse(args, Job(null, "", Augment))
-      .foreach{job => run(job, providerBuilder)}
+      .foreach { job => run(job, providerBuilder) }
   }
 
   trait Operation
   case object Augment extends Operation
   case object Destroy extends Operation
   case object Redeploy extends Operation
+  case object ListIdle extends Operation
 
-  object Operation{
-    val values = Vector(Augment, Destroy, Redeploy)
-    
+  object Operation {
+    val values = Vector(Augment, Destroy, Redeploy, ListIdle)
+
     def fromString(str: String): Option[Operation] = {
       values.find(_.toString.toLowerCase == str.toLowerCase)
     }
 
     implicit val operationRead: Read[Operation] =
-      Read.reads[Operation]{str =>
-        fromString(str).getOrElse(throw new Exception("Could not parse: "+str))
+      Read.reads[Operation] { str =>
+        fromString(str).getOrElse(throw new Exception("Could not parse: " + str))
       }
   }
 
   case class Job(
     configFile: File,
     clusterTag: String,
-    operation: Operation = Augment
-  )
+    operation: Operation = Augment)
 
   val parser = new scopt.OptionParser[Job]("Deployer") {
-    opt[File]('c', "config") required() valueName("<file>") action{ (x, c) =>
+    opt[File]('c', "config") required () valueName ("<file>") action { (x, c) =>
       c.copy(configFile = x)
     } text ("Cluster properties file ")
 
-    opt[String]('n', "name") required() valueName("<clusterName>") action { (x, c) =>
+    opt[String]('n', "name") required () valueName ("<clusterName>") action { (x, c) =>
       c.copy(clusterTag = x)
     } text ("Name of the cluster.  E.g. 'myCluster' if instances have been tagged with \"cluster:myCluster\".")
 
-    opt[Operation]('o', "operation") valueName("<operation>") action{ (x,c) =>
+    opt[Operation]('o', "operation") valueName ("<operation>") action { (x, c) =>
       c.copy(operation = x)
     } text ("Cluster operation to perform")
   }
-  
+
   def process(cmd: String): Int = {
-    log.info("About to run: "+cmd)
+    log.info("About to run: " + cmd)
     Process(cmd).!(processLog)
   }
-	def process(cmds: Seq[String]): Int = {
-	  log.info("About to run: "+cmds)
-	  Process(cmds).!(processLog)
-	}
-  
+  def process(cmds: Seq[String]): Int = {
+    log.info("About to run: " + cmds)
+    Process(cmds).!(processLog)
+  }
+
   def run(job: Job, providerBuilder: String => Provider): Unit = {
     log.info(job.toString)
     val config = FileUtils.readFileToString(job.configFile)
@@ -78,13 +78,12 @@ object Deployer {
     val nodes = allNodes.filter(_.clusterNameOpt == Some(job.clusterTag))
     log.info(s"Cluster has ${nodes.size} nodes (out of ${allNodes.size} instances): ")
     nodes.map("  " + _.toString).foreach(log.info)
-    
+
     assume(nodes.size > 0, s"Found 0 nodes in cluster ${job.clusterTag} (${allNodes.size} nodes overall)")
 
     val seeds = List(
       Util.getAssertOne(nodes.filter(_.seedRoleOpt == Some("seed1"))),
-      Util.getAssertOne(nodes.filter(_.seedRoleOpt == Some("seed2")))
-    )
+      Util.getAssertOne(nodes.filter(_.seedRoleOpt == Some("seed2"))))
 
     val ssh = new SSH(props.privateKeyPath)
     val rsync = new Rsync(props.privateKeyPath)
@@ -97,15 +96,34 @@ object Deployer {
         deploy(nodes)
       case Augment =>
         deploy(nodes)
+      case ListIdle =>
+        listIdle(nodes)
+    }
+
+    def listIdle(nodes: Set[Node]): Unit = {
+      //Note isrunning used in deploy also - abstract out or refactor?
+      def isRunningABC(node: Node): Boolean = {
+        val cmd = ssh.foregroundCommand(
+          provider.instanceUser,
+          node.ip,
+          Script.checkJavaRunning(props.applicationMain))
+        val exitCode = process(cmd)
+        exitCode == 0 //Assuming exit code of 0 means model is running
+      }
+      nodes
+        .foreach { node =>
+          if (!isRunningABC(node)) {
+            println(node)
+          }
+        }
     }
 
     def tearDown(nodes: Set[Node]): Unit = {
-      nodes.foreach{node =>
+      nodes.foreach { node =>
         process(ssh.foregroundCommand(
           provider.instanceUser,
           node.ip,
-          Script.killJava(props.applicationMain)
-        ))
+          Script.killJava(props.applicationMain)))
       }
     }
 
@@ -120,7 +138,7 @@ object Deployer {
         exitCode == 0 //Assuming exit code of 0 means model is running
       }
 
-      def upload(node: Node, props: Properties, runScriptPath: Path): Unit =  {
+      def upload(node: Node, props: Properties, runScriptPath: Path): Unit = {
         process(rsync(
           provider.instanceUser,
           node.ip,
@@ -145,8 +163,8 @@ object Deployer {
 
       nodes
         .foreach { node =>
-          log.info("Working on "+node)
-          if(!isRunningABC(node)){
+          log.info("Working on " + node)
+          if (!isRunningABC(node)) {
             val runScript: String = Script.startApplication(
               node.ip,
               props.vmExtraArgs,
@@ -154,7 +172,7 @@ object Deployer {
               seeds(0).ip,
               seeds(1).ip)
             val runScriptPath = Util.writeToTempFile(runScript, "run", ".sh")
-  
+
             upload(node, props, runScriptPath)
             execute(node, props, runScriptPath.getFileName.toString)
           }
