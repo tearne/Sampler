8c914f82a6892d243cc74e9f13d00058903fa52a
Tearne <tearne@gmail.com>
2016-07-02 22:02:58 +0100

    Property test for distribution from weights table

--- sampler-core/src/test/scala/sampler/distribution/DistributionTest.scala
+++ sampler-core/src/test/scala/sampler/distribution/DistributionTest.scala
@@ -7,17 +7,64 @@ import sampler.math.Random
 import org.scalatest.Matchers
 import org.scalatest.prop.Checkers
 import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
+import sampler.math.{Partition, AliasTable}
+import scala.collection.immutable.ListMap
 
 class DistributionTest 
     extends FreeSpec 
     with GeneratorDrivenPropertyChecks 
-//    with Checkers
     with Matchers {
   
-  "A distribution" - {
+  
+  
+  val weightsTable = {
+    val weightedStringTuple = 
+      for{
+        value <- Gen.listOfN(5, Gen.alphaChar).map(_.mkString)
+        weight <- arbitrary[Double].retryUntil(_ > 0.1)
+      } yield value -> weight
+      
+    for{
+      size <- Gen.choose(1, 100)
+      tuples <- Gen.listOfN(size, weightedStringTuple)
+    } yield ListMap(tuples: _*)
+  }
+  
+  "A distribution " - {
     implicit val r = Random
     
-    "Is a Samplable" - {
+    "can be built from" - {
+      "a weights table" in forAll(weightsTable){wtTable =>
+        def mockRandom = new Random{
+          def nextInt(n: Int) = 0
+          val it = (1 to 99).map(_ / 100.0).toIterator
+          def nextDouble() = it.next
+        }
+        
+        val actual = {
+          wtTable.toDistribution
+            .until(_.size == wtTable.size)
+            .sample(mockRandom)
+        }
+        
+        val expected = {
+          val partition = Partition.fromWeights(wtTable.values.toIndexedSeq)
+          val indexedValues = wtTable.keys.toIndexedSeq
+          val at = new AliasTable(partition)
+
+          (1 to wtTable.size).map{_ => 
+            indexedValues(at.next(mockRandom))
+          }
+        }
+        
+        assert(actual === expected)
+      }
+      "an indexed seq" in pending
+      "a function" in pending
+    }
+    
+    "is Samplable:" - {
       val oneToHundred = Gen.choose(1,1000)
       val intSet = Gen.containerOf[Set,Int](oneToHundred)
       
@@ -33,10 +80,12 @@ class DistributionTest
       }
       
       "Until" in forAll(oneToHundred){ (n: Int) =>
-        assertResult(List.fill(n)(1)){
-          CommonDistributions.always(1)
-            .until(_.size == n)
-            .sample
+        whenever(n > 0){
+          assertResult(List.fill(n)(1)){
+            CommonDistributions.always(1)
+              .until(_.size == n)
+              .sample
+          }
         }
       }
       
@@ -46,14 +95,19 @@ class DistributionTest
       		val it = makeIt(s)
       		Distribution.from(_ => it.next)
         }
-        forAll{(seqA: Seq[Int], seqB: Seq[Int]) =>
+        val operators = Gen.oneOf(
+          (a: Int, b: Int) => a + b,
+          (a: Int, b: Int) => a * b,
+          (a: Int, b: Int) => Math.max(a, b)
+        )
+        forAll(arbitrary[Seq[Int]],arbitrary[Seq[Int]],operators){(seqA: Seq[Int], seqB: Seq[Int], operator: (Int, Int) => Int) =>
           whenever(seqA.size > 0 && seqB.size > 0){
             val aLength = seqA.size
-            val result = makeDist(seqA).combine(makeDist(seqB))(_ + _)
+            val result = makeDist(seqA).combine(makeDist(seqB))(operator)
               .until(_.size == aLength)
               .sample
             val expected = makeIt(seqA).zip(makeIt(seqB))
-              .map{case (a,b) => a + b}
+              .map{case (a,b) => operator(a,b)}
               .take(aLength)
               .toSeq
             assert(result == expected)
@@ -61,8 +115,13 @@ class DistributionTest
         }
       }
       
-      "Extends Monad" in {
-        pending
+      "Monad" - {
+        "FlatMap" in {
+          pending
+        }
+        "Pure" in {
+          pending
+        }
       }
     }
   }
