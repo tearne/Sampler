984a876152496d84562a0b4ffb7d4d6d27d55fa6
Tearne <tearne@gmail.com>
2016-07-17 22:30:18 +0100

    Flatten out tests and refine, but weights table failing

--- sampler-core/src/test/scala/sampler/distribution/DistributionTest.scala
+++ sampler-core/src/test/scala/sampler/distribution/DistributionTest.scala
@@ -16,9 +16,7 @@ class DistributionTest
     with GeneratorDrivenPropertyChecks 
     with Matchers {
   
-  
-  
-  val weightsTable = {
+  val weightsTableGen = {
     val weightedStringTuple = 
       for{
         value <- Gen.listOfN(5, Gen.alphaChar).map(_.mkString)
@@ -31,97 +29,120 @@ class DistributionTest
     } yield ListMap(tuples: _*)
   }
   
-  "A distribution " - {
-    implicit val r = Random
-    
-    "can be built from" - {
-      "a weights table" in forAll(weightsTable){wtTable =>
-        def mockRandom = new Random{
-          def nextInt(n: Int) = 0
-          val it = (1 to 99).map(_ / 100.0).toIterator
-          def nextDouble() = it.next
-        }
-        
-        val actual = {
-          wtTable.toDistribution
-            .until(_.size == wtTable.size)
-            .sample(mockRandom)
-        }
-        
-        val expected = {
-          val partition = Partition.fromWeights(wtTable.values.toIndexedSeq)
-          val indexedValues = wtTable.keys.toIndexedSeq
-          val at = new AliasTable(partition)
+  implicit val r = Random
 
-          (1 to wtTable.size).map{_ => 
-            indexedValues(at.next(mockRandom))
-          }
-        }
-        
-        assert(actual === expected)
+  def mockRandom = new Random{
+	  def nextInt(n: Int) = 0
+			  val it = (1 to 99).map(_ / 100.0).toIterator
+			  def nextDouble() = it.next
+  }
+  
+  "Build from weights table" in forAll(weightsTableGen){wtTable =>
+    whenever(wtTable.values.exists(_ > 0)){
+      val actual = {
+        wtTable.toDistribution
+          .until(_.size == wtTable.size)
+          .sample(mockRandom)
       }
-      "an indexed seq" in pending
-      "a function" in pending
+    
+      val expected = {
+        val partition = Partition.fromWeights(wtTable.values.toIndexedSeq)
+        val indexedValues = wtTable.keys.toIndexedSeq
+        val at = new AliasTable(partition)
+  
+        (1 to wtTable.size).map{_ => 
+          indexedValues(at.next(mockRandom))
+        }
+      }
+      
+      assert(actual === expected)
+    }
+  }
+  
+  "Build from indexed seq" in pending
+  "Build form function" in pending
+  
+  "Is Samplable:" - {
+    val oneToHundred = Gen.choose(1,1000)
+    val intSet = Gen.containerOf[Set,Int](oneToHundred)
+    
+    "Filtering" in forAll(intSet, intSet){ (pool: Set[Int], reject: Set[Int]) =>
+      whenever(pool.size > 0){
+        val filteredSamples = CommonDistributions.uniform(pool.toIndexedSeq)
+          .filter{v => !reject.contains(v)}
+          .until(_.size == 100)
+          .sample
+          .toSet
+        assert(filteredSamples.intersect(reject).size === 0)
+      }
     }
     
-    "is Samplable:" - {
-      val oneToHundred = Gen.choose(1,1000)
-      val intSet = Gen.containerOf[Set,Int](oneToHundred)
-      
-      "Filtering" in forAll(intSet, intSet){ (pool: Set[Int], reject: Set[Int]) =>
-        whenever(pool.size > 0){
-          val filteredSamples = CommonDistributions.uniform(pool.toIndexedSeq)
-            .filter{v => !reject.contains(v)}
-            .until(_.size == 100)
+    "Until" in forAll(oneToHundred){ (n: Int) =>
+      whenever(n > 0){
+        assertResult(List.fill(n)(1)){
+          CommonDistributions.always(1)
+            .until(_.size == n)
             .sample
-            .toSet
-          assert(filteredSamples.intersect(reject).size === 0)
         }
       }
-      
-      "Until" in forAll(oneToHundred){ (n: Int) =>
-        whenever(n > 0){
-          assertResult(List.fill(n)(1)){
-            CommonDistributions.always(1)
-              .until(_.size == n)
-              .sample
+    }
+    
+    "Combining" in {
+      def makeIt(s: Seq[Int]) = Stream.continually(s).flatten.iterator
+      def makeDist(s: Seq[Int]) = {
+    		val it = makeIt(s)
+    		Distribution.from(_ => it.next)
+      }
+      val operators = Gen.oneOf(
+        (a: Int, b: Int) => a + b,
+        (a: Int, b: Int) => a * b,
+        (a: Int, b: Int) => Math.max(a, b)
+      )
+      forAll(arbitrary[Seq[Int]],arbitrary[Seq[Int]],operators){(seqA: Seq[Int], seqB: Seq[Int], operator: (Int, Int) => Int) =>
+        whenever(seqA.size > 0 && seqB.size > 0){
+          val aLength = seqA.size
+          val result = makeDist(seqA).combine(makeDist(seqB))(operator)
+            .until(_.size == aLength)
+            .sample
+          val expected = makeIt(seqA).zip(makeIt(seqB))
+            .map{case (a,b) => operator(a,b)}
+            .take(aLength)
+            .toSeq
+          assert(result == expected)
+        }
+      }
+    }
+    
+    "Monad" - {
+      "FlatMap" in {
+        val ten = 10
+        val indexGen = Gen.listOfN(ten, Gen.choose(0, 9))
+        val matrixGen = Gen.listOfN(ten, Gen.listOfN(ten, Gen.alphaChar))
+        def dist[T](seq: Seq[T]) = {
+          val it = seq.iterator
+          Distribution.from { _ => it.next }
+        }
+        forAll(indexGen, matrixGen){(index: Seq[Int], matrix: Seq[Seq[Char]]) => 
+          whenever(index.size > 0 && matrix.size > 0 && !matrix.exists(_.size == 0)){
+            val indexDist: Distribution[Int] = dist(index)
+            val distSeq: Seq[Distribution[Char]] = matrix.map(dist)
+            
+            val result = indexDist.flatMap{i => distSeq(i % distSeq.size)}
+                .until(_.size == index.size)
+                .sample
+            
+            val expected = {
+             	val matrixDist1: Seq[Distribution[Char]] = matrix.map(dist)
+              index.map{i => matrixDist1(i % distSeq.size).sample}
+                .take(index.size)
+            }
+              
+            assert(result === expected)
           }
         }
       }
-      
-      "Combining" in {
-        def makeIt(s: Seq[Int]) = Stream.continually(s).flatten.iterator
-        def makeDist(s: Seq[Int]) = {
-      		val it = makeIt(s)
-      		Distribution.from(_ => it.next)
-        }
-        val operators = Gen.oneOf(
-          (a: Int, b: Int) => a + b,
-          (a: Int, b: Int) => a * b,
-          (a: Int, b: Int) => Math.max(a, b)
-        )
-        forAll(arbitrary[Seq[Int]],arbitrary[Seq[Int]],operators){(seqA: Seq[Int], seqB: Seq[Int], operator: (Int, Int) => Int) =>
-          whenever(seqA.size > 0 && seqB.size > 0){
-            val aLength = seqA.size
-            val result = makeDist(seqA).combine(makeDist(seqB))(operator)
-              .until(_.size == aLength)
-              .sample
-            val expected = makeIt(seqA).zip(makeIt(seqB))
-              .map{case (a,b) => operator(a,b)}
-              .take(aLength)
-              .toSeq
-            assert(result == expected)
-          }
-        }
-      }
-      
-      "Monad" - {
-        "FlatMap" in {
-          pending
-        }
-        "Pure" in {
-          pending
-        }
+      "Pure" in {
+        pending
       }
     }
   }
