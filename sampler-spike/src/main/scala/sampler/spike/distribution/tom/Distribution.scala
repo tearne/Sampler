e6804d4ba095b0ecbd9348fa1fd45ec4caf65111
Tearne <tearne@gmail.com>
2016-08-29 20:49:54 +0100

    Remove old distribution typeclass from spike now it's in core.

--- sampler-spike/src/main/scala/sampler/spike/distribution/tom/Distribution.scala
+++
@@ -1,8 +0,0 @@
-package sampler.spike.distribution.tom
-
-import cats.Monad
-import scala.language.higherKinds
-
-trait Distribution[D[_]] extends Monad[D]{
-  def fromTable[A](weights: Map[A, Double]): D[A]
-}
