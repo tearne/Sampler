daf6a805a6777160e815e2660d6a35505688bda9
tearne <tearne@gmail.com>
2016-09-14 10:36:13 +0000

    Fleshing out new root actor

--- sampler-abc/src/main/scala/sampler/abc/refactor/RootActor.scala
+++ sampler-abc/src/main/scala/sampler/abc/refactor/RootActor.scala
@@ -1,7 +1,7 @@
 package sampler.abc.refactor
 
-import akka.actor.Actor
 import sampler.abc.Generation
+import sampler.abc.actor.main.{ReportCompleted, ScoredParticles}
 
 object Messages {
   case class Start[P](initGen: Generation[P])
@@ -10,14 +10,40 @@ object Messages {
 class RootActor[P](businessLogic: BusinessLogic) extends Actor {
   import Messages._
 
-  def receive: Receive = {
+  def receive = idle()
+
+  def idle: Receive = {
     case s: Start[P] =>
-      businessLogic.init(s.initGen)
+      context.become(businessLogic.init(s.initGen))
+    case _ => //log warning
+
+
+
+  def gathering: Receive = {
+    case Failed =>
+    case scored: ScoredParticles[P] =>
+    case weighted: WeighedParticles[P] =>
+    case MixNow =>
+    case mixP: MixPayload[P] =>
+    case ReportCompleted =>
   }
 
+  def flushing: Receive ={
+    case ScoredParticles[P] =>
+    case MixNow =>
+    case fc: FlushComplete =>
+    case ReportCompleted =>
+  }
 
+  def waitingForShutdown: Receive ={
+    case ReportCompleted =>
+  }
 }
 
 class BusinessLogic{
 
+}
+
+object BusinessLogic{
+  def init(): BusinessLogic = ???
 }
