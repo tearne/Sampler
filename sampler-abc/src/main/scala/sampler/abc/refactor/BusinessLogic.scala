819b4ce2df44dbbc2fff3f16832e34b3be423387
tearne <tearne@gmail.com>
2016-09-23 20:52:57 +0000

    Stubbed most of the root actor and business logic now

--- sampler-abc/src/main/scala/sampler/abc/refactor/BusinessLogic.scala
+++ sampler-abc/src/main/scala/sampler/abc/refactor/BusinessLogic.scala
@@ -6,44 +6,62 @@ import sampler.abc.actor.main.{MixPayload, ScoredParticles, WeighedParticles}
 import sampler.abc.refactor.Messages.Start
 
 class BusinessLogic[P] {
-  def start(startMsg: Start[P], requestor: ActorRef): State[P] ={
-    // Build initial state
-    // start mixing timer
-    //
+  def buildInitialState(startMsg: Start[P], requestor: ActorRef): State[P] ={
     ???
   }
 
   def workerFailed(state: State[P], worker: ActorRef): Unit = {
-    // Log the error
+    // Log the failure
     // Allocate a repeat job to the same worker
   }
 
-  def addNewLocallyScoredParticles(state: State[P], scored: ScoredParticles[P], sender: ActorRef): State[P] = {
-    // filter and queue the new scored particles
-    // report on the new state
-    // send a weigh job to the worker that send the particles
+  def addLocallyScoredParticles(state: State[P], scored: ScoredParticles[P], sender: ActorRef): State[P] = {
+    // Filter and queue the new scored particles
+    // Report on the new state
+    // Send a weigh job to the worker that sent the particles
+    ???
+  }
+
+  def addScoredFromMixing(mixP: MixPayload[P], sender: ActorRef): State[P] = {
+    // Update state by adding good particles
+    // Report new state
     ???
   }
 
   def addNewWeighted(state: State[P], weighed: WeighedParticles[P], sender: ActorRef): State[P] = {
-    // add the weighted particles to the state
-    // do a report
-    // If state.dueToFlush
-    //       tell all local worker to abort via the broadcaster
-    //       report gen finished
-    //       tell the flusher to flush
-    // Else
-    //       nothing
-    //return the new state
+    // Add the weighted particles to the state
+    // Do a report
     ???
   }
 
-  def doMixing(state: State[P]): Unit {
+  def doGenerationFlush(state: State[P]) {
+    // Tell all local workers to abort via the broadcaster
+    // Report gen finished
+    // Tell the flusher to flush newState
+  }
+
+  def allocateWork(state: State[P], worker: ActorRef) {
+    //if stuff waiting to be weighed
+    //  allocate weighing job
+    //else
+    //  allocate particle generating job
+  }
+
+  def doMixing(state: State[P]) {
     // via the broadcaster
   }
 
-  def addScoredFromMixPayload(mixP: MixPayload[P], sender: ActorRef): State = {
-    
+  def terminate(state: State[P]) {
+    // Send Abort to all workers
+    // Send message to generate a report
   }
 
-}
+  def startNewGeneration(state: State[P]) {
+    // Send GenerateParticlesFrom message to workers via broadcaster
+    // Send message to generate a report
+  }
+
+  def sendResultToClient(state: State[P]) {
+
+  }
+ }
--- sampler-abc/src/main/scala/sampler/abc/refactor/RootActor.scala
+++ sampler-abc/src/main/scala/sampler/abc/refactor/RootActor.scala
@@ -12,13 +12,17 @@ class RootActor[P](
                   ) extends Actor {
   import Messages._
 
-  //TODO put the mixing timer in a separate actor
+  //TODO put the mixing timer in a separate child actor
+  // which is always firing (for simplicity)
 
   def receive: Receive = idle()
 
   def idle(): Receive = {
-    case s: Start[P] =>
-      context.become(gathering(logic.start(s, sender)))
+    case MixNow =>                // Ignored
+    case start: Start[P] =>
+      val initialState = logic.buildInitialState(start, sender)
+      logic.startNewGeneration(initialState)
+      context.become(gathering(initialState))
     case _ => //TODO log warning
   }
 
@@ -26,14 +30,18 @@ class RootActor[P](
     case Failed =>
       logic.workerFailed(state, sender)
     case scored: ScoredParticles[P] =>
-      context.become(gathering(logic.addNewLocallyScoredParticles(state, scored, sender)))
+      context.become(gathering(logic.addLocallyScoredParticles(state, scored, sender)))
+    case mixP: MixPayload[P] =>
+      context.become(gathering(logic.addScoredFromMixing(mixP, sender)))
     case weighted: WeighedParticles[P] =>
       val newState = logic.addNewWeighted(state, weighted, sender)
-      if(newState.dueToFlush) context.become(waitingForFlushComplete(newState))
-    case MixNow =>
-      logic.doMixing(state)
-    case mixP: MixPayload[P] =>
-      context.become(gathering(state.addScoredFromMixPayload(mixP, sender)))
+      if(newState.dueToFlush) {
+        logic.doGenerationFlush(newState)
+        context.become(waitingForFlushComplete(newState))
+      } else {
+        logic.allocateWork(newState, sender)
+      }
+    case MixNow => logic.doMixing(state)
     case ReportCompleted => //Do nothing, just don't get upset by the message
     case _ => //TODO log warning
   }
@@ -42,18 +50,18 @@ class RootActor[P](
     case _: ScoredParticles[P] => // Ignored
     case MixNow =>                // Ignored
     case ReportCompleted =>       // Ignored
-    case fc: FlushCompleteTerminate[P] =>
-      //Stop child actors, send to reporter
-      context.become(waitingForShutdown(fc.state.terminate()))
-    case fc: FlushCompleteContinue[P] =>
-      //
-      context.become(gathering(fc.state.startNextGeneration()))
+    case flushed: FlushCompleteTerminate[P] =>
+      logic.terminate(flushed.state)
+      context.become(waitingForShutdown(flushed.state.terminate()))
+    case flushed: FlushCompleteContinue[P] =>
+      logic.startNewGeneration(flushed.state)
+      context.become(gathering(flushed.state))
     case _ => //TODO log warning
   }
 
-  def waitingForShutdown(state: BusinessLogic): Receive ={
+  def waitingForShutdown(state: State[P]): Receive ={
     case ReportCompleted =>
-      // Send final results to clinet
+      logic.sendResultToClient(state)
     case _ => //TODO log warning
   }
 }
