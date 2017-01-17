366432fe48a61271bc9ec4406ad35e0ec8b64806
tearne <tearne@gmail.com>
2017-01-17 13:38:28 +0000

    Tidy, add titles

--- sampler-examples/src/main/scala/sampler/example/abc/flockMortality/ODETest.scala
+++ sampler-examples/src/main/scala/sampler/example/abc/flockMortality/ODETest.scala
@@ -15,17 +15,17 @@ object ODETest extends App {
   val D0 = 0
   val y0 = Array(S0, E0, 0.0, 0.0, D0)
 
-
   val numDays = 10
   val params = Parameters(
     0.05, //beta
     0.25,//eta
     0.8, //gamma
     0.1, //delta
-    0.5, //sigma
-    0.4,  //sigma2,
-    IndexedSeq(9)  //offset
+    0, //sigma - irrelevant
+    0,  //sigma2 - irrelevant
+    null  //offset per shed - irrelevant
   )
+
   val eggCoeff = 0.8
   val baselineMortalityRate = 0
 
@@ -52,17 +52,15 @@ object ODETest extends App {
   FileUtils.writeStringToFile(wDir.resolve("solved.json").toFile, jsonStr)
 
 
-  val script = """
+  val script = s"""
 lapply(c("ggplot2", "reshape2", "deSolve", "jsonlite"), require, character.only=T)
 pdf("test.pdf", width=4.13, height=2.91)
 
 params = c(
-  beta = 0.05,
-  eta = 0.25,
-  gamma = 0.8,
-  delta = 0.1,
-  sigma = 0.5,
-  sigma2 = 0.4
+  beta = ${params.beta},
+  eta = ${params.eta},
+  gamma = ${params.gamma},
+  delta =${params.delta}
 )
 
 Y0 = c(
@@ -91,12 +89,14 @@ out <- ode(y = Y0, times = times, func = dY, parms = params)
 head(out)
 data = melt(as.data.frame(out), id="time")
 ggplot(data, aes(x=time, y=value, colour=variable)) +
-geom_line()
+  geom_line() +
+  ggtitle("R ODE Solution")
 
 
 data = melt(as.data.frame(fromJSON("solved.json")), id="day")
 ggplot(data, aes(x=day, y=value, colour=variable)) +
-  geom_line()
+  geom_line() +
+  ggtitle("Scala ODE Solution")
 dev.off()
   """
 
